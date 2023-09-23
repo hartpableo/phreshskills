e93ecd03f086493e85d302a3f0abd9bf91f93ede
Hart <hart.hpwebdev@gmail.com>
2023-09-23 21:36:45 +0800

    Footer CTA

--- app/views/pages/index.view.php
+++ app/views/pages/index.view.php
@@ -1,110 +1,114 @@
 <?php get_template_part('header'); ?>
+
 <section id="hero">
-    <div class="has-overlay relative max-w-[1536px] bg-fixed bg-cover mx-auto px-3 text-center relative overflow-hidden isolate bg-black py-20 lg:py-9 text-white min-h-[45vh] md:min-h-[60vh] flex flex-col justify-center items-center text-md lg:text-lg" style="background-image: url(<?php echo imagePath('hero-bg.webp'); ?>);">
+  <div class="has-overlay relative max-w-[1536px] bg-fixed bg-cover mx-auto px-3 text-center relative overflow-hidden isolate bg-black py-20 lg:py-9 text-white min-h-[45vh] md:min-h-[60vh] flex flex-col justify-center items-center text-md lg:text-lg" style="background-image: url(<?php echo imagePath('hero-bg.webp'); ?>);">
 
-      <h1 
-      class="font-secondary text-4xl lg:text-6xl font-bold mb-8"
-      >Welcome to <span class="text-orange-500"><?php echo APP_NAME; ?></span>!</h1>
-      
-      <p class="max-w-2xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat natus incidunt recusandae, enim quisquam perspiciatis veniam dolore in accusamus officiis!</p>
-
-      <div class="flex justify-center items-start gap-4 flex-wrap mt-12">
-
-        <a 
-        href="#" 
-        class="transition-all inline-flex justify-center items-center font-bold py-2 px-6 bg-orange-500 border border-solid border-orange-500 hover:border-blue-900 text-white hover:bg-blue-900 text-xl min-w-[12em]"
-        >Start Applying</a>
-
-        <a 
-        href="#" 
-        class="transition-all inline-flex justify-center items-center font-bold py-2 px-6 bg-transparent border border-solid border-white text-white hover:border-blue-900 hover:bg-blue-900 text-xl min-w-[12em]"
-        >Post Jobs</a>
-
-      </div>
-
-    </div>
-  </section>
-
-  <section id="jobseekers-list" class="py-20">
-
-    <h2 class="container font-secondary text-6xl mb-20 font-bold text-blue-900">Talents</h2>
-
-    <?php if (!empty($jobseekers)) : ?>
-
-    <div class="container grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+    <h1 
+    class="font-secondary text-4xl lg:text-6xl font-bold mb-8"
+    >Welcome to <span class="text-orange-500"><?php echo APP_NAME; ?></span>!</h1>
     
-      <?php foreach ($jobseekers as $jobseeker) : ?>
+    <p class="max-w-2xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat natus incidunt recusandae, enim quisquam perspiciatis veniam dolore in accusamus officiis!</p>
 
-      <?php $nameSlug = Core\Slugifier::slugify($jobseeker['name']); ?>
-        
-      <article
-      role="article"
-      aria-labelledby="title--<?php echo $nameSlug; ?>"
-      class="block rounded-lg bg-blue-100 text-left mb-20 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] p-6">
-
-        <img src="https://picsum.photos/200/200" width="200" height="200" alt="..." 
-          class="rounded-full w-[6.6rem] h-[6.6rem] mt-[-4.85rem] mb-3" aria-hidden="true" role="presentation">
-
-        <h3
-          id="title--<?php echo $nameSlug; ?>"
-          class="mb-1 text-2xl font-bold leading-tight font-secondary">
-          <a href="#" class="text-neutral-800 hover:text-orange-500 transition-all"><?php echo $jobseeker['name']; ?></a>
-        </h3>
-        <p
-          class="mb-3 text-md font-semibold leading-tight text-neutral-500 text-blue-900">
-          <?php echo $jobseeker['position']; ?>
-        </p>
-        <p
-          class="mb-2 text-base leading-tight text-neutral-800">
-          <?php echo excerpt($jobseeker['summary'], 125); ?>
-        </p>
-
-        <?php
-          $skills = explode(', ', $jobseeker['skills']);
-          if (!empty($skills)) :
-        ?>
-
-        <ul aria-label="Skills" class="flex justify-start items-start flex-wrap gap-1 mb-2">
-          
-          <?php foreach($skills as $skill) : ?>
-          
-          <li class="inline-block">
-            <a 
-              class="p-1 px-2 font-semibold text-xs rounded bg-gray-500 text-neutral-50 hover:bg-blue-700 transition-all" 
-              href="#"
-            ><?php echo ucwords($skill); ?></a>
-          </li>
-
-          <?php endforeach; ?>
-        
-        </ul>
-
-        <?php endif; ?>
-
-        <div class="mt-6 flex justify-start items-start flex-wrap gap-x-4 gap-y-2">
-          <a
-            href="#"
-            class="pointer-events-auto inline-block cursor-pointer rounded text-base font-bold leading-normal text-primary text-blue-500 transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 hover:text-orange-500 hover:border-orange-500 transition-all px-2 border-2 border-solid border-blue-500 rounded transition-all">
-            View <span class="sr-only">Hart's </span>Profile
-          </a>
-        </div>
-      </article>
-      
-      <?php endforeach; ?>
-    
-    </div>
-
-    <?php endif; ?>
-
-    <div class="mt-8 flex gap-x-2 gap-y-3 justify-center items-start flex-wrap">
+    <div class="flex justify-center items-start gap-4 flex-wrap mt-12">
 
       <a 
-      href="#"
-      class="inline-flex px-10 py-2 text-xl font-bold text-white bg-blue-900 hover:bg-orange-500 rounded transition-all"
-      >View More <span class="sr-only">Jobseekers</span></a>
+      href="#" 
+      class="transition-all inline-flex justify-center items-center font-bold py-2 px-6 bg-orange-500 border border-solid border-orange-500 hover:border-blue-900 text-white hover:bg-blue-900 text-xl min-w-[12em]"
+      >Start Applying</a>
+
+      <a 
+      href="#" 
+      class="transition-all inline-flex justify-center items-center font-bold py-2 px-6 bg-transparent border border-solid border-white text-white hover:border-blue-900 hover:bg-blue-900 text-xl min-w-[12em]"
+      >Post Jobs</a>
 
     </div>
 
-  </section>
+  </div>
+</section>
+
+<section id="jobseekers-list" class="py-20">
+
+  <h2 class="container font-secondary text-6xl mb-20 font-bold text-blue-900">Talents</h2>
+
+  <?php if (!empty($jobseekers)) : ?>
+
+  <!-- Jobseekers Grid -->
+  <div class="container grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+  
+    <?php foreach ($jobseekers as $jobseeker) : ?>
+
+    <?php $nameSlug = Core\Slugifier::slugify($jobseeker['name']); ?>
+      
+    <article
+    role="article"
+    aria-labelledby="title--<?php echo $nameSlug; ?>"
+    class="block rounded-lg bg-blue-100 text-left mb-20 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] p-6">
+
+      <img src="https://picsum.photos/200/200" width="200" height="200" alt="..." 
+        class="rounded-full w-[6.6rem] h-[6.6rem] mt-[-4.85rem] mb-3" aria-hidden="true" role="presentation">
+
+      <h3
+        id="title--<?php echo $nameSlug; ?>"
+        class="mb-1 text-2xl font-bold leading-tight font-secondary">
+        <a href="#" class="text-neutral-800 hover:text-orange-500 transition-all"><?php echo $jobseeker['name']; ?></a>
+      </h3>
+      <p
+        class="mb-3 text-md font-semibold leading-tight text-neutral-500 text-blue-900">
+        <?php echo $jobseeker['position']; ?>
+      </p>
+      <p
+        class="mb-2 text-base leading-tight text-neutral-800">
+        <?php echo excerpt($jobseeker['summary'], 125); ?>
+      </p>
+
+      <?php
+        $skills = explode(', ', $jobseeker['skills']);
+        if (!empty($skills)) :
+      ?>
+
+      <ul aria-label="Skills" class="flex justify-start items-start flex-wrap gap-1 mb-2">
+        
+        <?php foreach($skills as $skill) : ?>
+        
+        <li class="inline-block">
+          <a 
+            class="p-1 px-2 font-semibold text-xs rounded bg-gray-500 text-neutral-50 hover:bg-blue-700 transition-all" 
+            href="#"
+          ><?php echo ucwords($skill); ?></a>
+        </li>
+
+        <?php endforeach; ?>
+      
+      </ul>
+
+      <?php endif; ?>
+
+      <div class="mt-6 flex justify-start items-start flex-wrap gap-x-4 gap-y-2">
+        <a
+          href="#"
+          class="pointer-events-auto inline-block cursor-pointer rounded text-base font-bold leading-normal text-primary text-blue-500 transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 hover:text-orange-500 hover:border-orange-500 transition-all px-2 border-2 border-solid border-blue-500 rounded transition-all">
+          View <span class="sr-only">Hart's </span>Profile
+        </a>
+      </div>
+    </article>
+    
+    <?php endforeach; ?>
+  
+  </div>
+
+  <!-- End of Jobseekers Grid -->
+
+  <?php endif; ?>
+
+  <div class="mt-8 flex gap-x-2 gap-y-3 justify-center items-start flex-wrap">
+
+    <a 
+    href="#"
+    class="inline-flex px-10 py-2 text-xl font-bold text-white bg-blue-900 hover:bg-orange-500 rounded transition-all"
+    >View More <span class="sr-only">Jobseekers</span></a>
+
+  </div>
+
+</section>
 
 <?php get_template_part('footer'); ?>
--- app/views/partials/footer.view.php
+++ app/views/partials/footer.view.php
@@ -1,5 +1,31 @@
 </main>
 
+<section class="mb-3">
+  <div class="has-overlay has-overlay-darker relative max-w-[1536px] bg-fixed bg-cover mx-auto px-3 text-center relative overflow-hidden isolate bg-black py-20 text-white flex flex-col justify-center items-center text-md lg:text-lg" style="background-image: url(<?php echo imagePath('hero-bg.webp'); ?>);">
+
+    <h2
+    class="sr-only"
+    >Why launch this platform?</h2>
+    
+    <p class="max-w-2xl font-bold text-5xl leading-tight">This platform was made to help <span class="text-orange-500">Filipinos</span> jump-start their careers and for <span class="text-blue-500">companies or businesses worldwide</span> to find the right candidate for their team!</p>
+
+    <div class="flex justify-center items-start gap-4 flex-wrap mt-12">
+
+      <a 
+      href="#" 
+      class="transition-all inline-flex justify-center items-center font-bold py-2 px-6 bg-orange-500 border border-solid border-orange-500 hover:border-blue-900 text-white hover:bg-blue-900 text-xl min-w-[12em]"
+      >Frequently Asked Questions</a>
+
+      <a 
+      href="#" 
+      class="transition-all inline-flex justify-center items-center font-bold py-2 px-6 bg-transparent border border-solid border-white text-white hover:border-blue-900 hover:bg-blue-900 text-xl min-w-[12em]"
+      >About the Developer</a>
+
+    </div>
+
+  </div>
+</section>
+
 <footer role="contentinfo"
   class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
   <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
