cd05f0dad0e6a6c1a6630fbfc1e4c94ce5248ca7
Hart <hart.hpwebdev@gmail.com>
2023-09-23 19:51:09 +0800

    403 page

--- app/views/status_codes/403.view.php
+++ app/views/status_codes/403.view.php
@@ -1,3 +1,8 @@
 <?php get_template_part('header'); ?>
-<h1 class="w-100 text-center">403 | You are not authorized to access this page!</h1>
+
+<div class="container text-center">
+  <h1 class="w-100 font-secondary font-bold text-6xl">403 | You are not authorized to access this page!</h1>
+  <a href="/" class="inline-flex justify-between text-white items-center mt-20 font-bold rounded py-2 px-4 bg-blue-900 hover:bg-orange-500 text-xl">Go back</a>
+</div>
+
 <?php get_template_part('footer'); ?>
