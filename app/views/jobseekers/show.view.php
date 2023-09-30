61fd61dfe4c764adb356406489f088e193e1c914
Hart <hart.hpwebdev@gmail.com>
2023-09-30 13:17:27 +0800

    DB backup

--- app/views/jobseekers/show.view.php
+++ app/views/jobseekers/show.view.php
@@ -2,7 +2,7 @@
 
 <section class="has-overlay bg-fixed bg-center bg-cover relative isolate py-5 lg:py-10 h-full" style="background-image: url(<?php echo image_uri('hero-bg.webp'); ?>);background-repeat: no-repeat;">
   <div class="container max-w-[52rem] text-gray-200">
-    <div class="rounded-xl border-2 border-solid border-gray-300 py-6 px-10 overflow-auto">
+    <div class="rounded-xl border-2 border-solid border-gray-300 py-6 px-10 overflow-auto has-overlay-light relative overflow-hidden">
 
       <img src="https://picsum.photos/200/200" alt="<?php echo "Photo of {$jobseeker['name']}"; ?>" loading="lazy" width="200" height="200" class="rounded-full mr-5 mb-5 lg:float-left max-w-full max-h-auto aspect-ratio">
 
@@ -10,7 +10,7 @@
 
       <ul aria-label="Jobseeker's Profile">
         
-        
+
 
       </ul>
 
