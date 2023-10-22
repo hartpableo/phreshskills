00a37b0f5f727037a2970df2828a98853995f762
Hart <hart.hpwebdev@gmail.com>
2023-10-22 08:09:10 +0800

    alerts

--- app/views/partials/alerts.view.php
+++ app/views/partials/alerts.view.php
@@ -1,11 +1,10 @@
 <?php
   use Core\Session;
 ?>
-<?php //dd($_SESSION); ?>
 
 <?php if (Session::has('message')) : ?>
   <?php foreach (Session::get('message') as $message) : ?>
-    <div class="alert fixed w-full top-[10vh] rounded-lg flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50" role="alert">
+    <div class="alert fixed z-50 w-full top-[10vh] max-w-xl left-1/2 translate-x-[-50%] rounded-lg flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50" role="alert">
       <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" role="presentation" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"></path>
       </svg>
@@ -15,7 +14,7 @@
 <?php endif; ?>
 
 <?php if (Session::has('error-message')) : ?>
-  <div class="alert fixed w-full top-[10vh] rounded-lg flex items-center p-4 mb-4 text-green-800 border-t-4 border-red-300 bg-red-50" role="alert">
+  <div class="alert fixed z-50 w-full top-[10vh] max-w-xl left-1/2 translate-x-[-50%] rounded-lg flex items-center p-4 mb-4 text-green-800 border-t-4 border-red-300 bg-red-50" role="alert">
     <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" role="presentation" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"></path>
     </svg>
