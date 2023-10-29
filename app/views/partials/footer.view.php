f742760c5ef6a8bcecda7fa7e4bb4d86640551c0
Hart <hart.hpwebdev@gmail.com>
2023-10-29 12:18:43 +0800

    feedback button

--- app/views/partials/footer.view.php
+++ app/views/partials/footer.view.php
@@ -1,6 +1,13 @@
 </main>
 
 <footer role="contentinfo">
+
+  <a href="mailto:pableoh@gmail.com?subject=Feedback%20on%20the%20Phreshskills%20Platform"
+     class="floating fixed bottom-1 lg:bottom-8 left-0 lg:left-6 rounded hover:bg-white hover:text-black text-sm px-2 py-1 lg:px-4 lg:py-2 block bg-green-500 duration-300 text-white"
+  >
+    <p class="text-sm font-bold">Leave a Feedback!</p>
+  </a>
+
   <div class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
     <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
       &copy; 2023 Copyright | Made with ❤️ by 
