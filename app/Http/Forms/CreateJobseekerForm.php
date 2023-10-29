cc4a728315150e0959fbbc866873172c5bb3df49
Hart <hart.hpwebdev@gmail.com>
2023-10-29 14:06:51 +0800

    handle birth date

--- app/Http/Forms/CreateJobseekerForm.php
+++ app/Http/Forms/CreateJobseekerForm.php
@@ -19,6 +19,7 @@ class CreateJobseekerForm extends Form
     if (!Validator::string($attributes['password'], 7, INF)) $this->errors['password_error'] = 'Password is invalid (Must be at least 7 characters).';
     if (!Validator::check_if_empty($attributes['rate'])) $this->errors['rate_error'] = 'Please add your desired salary rate. :)';
     if (!Validator::check_if_empty($attributes['position'])) $this->errors['position_error'] = 'Please state your job position or the job position you are after. :)';
+    if (!Validator::check_if_empty($attributes['birth_date'])) $this->errors['birth_date_error'] = 'Please state your job position or the job position you are after. :)';
     if (!Validator::string($attributes['summary'], 7, 8000)) $this->errors['summary_error'] = 'Summary length is invalid.';
     if (!Validator::string($attributes['skills'], 1, INF)) $this->errors['skills_error'] = 'Please specify your current skills to be able to help employers see what you can do. :)';
     if (!Validator::file($attributes['profile_photo'])) $this->errors['profile_photo_error'] = 'Please upload your profile picture.';
@@ -42,8 +43,8 @@ class CreateJobseekerForm extends Form
 
     App::resolve(Database::class)
         ->query(
-          'insert into jobseekers(name, email, password, profile_photo, salary_type, skills, summary, rate, position, work_background_company_name, work_background_position, work_background_duration) 
-          values(:name, :email, :password, :profile_photo, :salary_type, :skills, :summary, :rate, :position, :work_background_company_name, :work_background_position, :work_background_duration)',
+          'insert into jobseekers(name, email, password, birth_date, profile_photo, salary_type, skills, summary, rate, position, work_background_company_name, work_background_position, work_background_duration) 
+          values(:name, :email, :password, :birth_date, :profile_photo, :salary_type, :skills, :summary, :rate, :position, :work_background_company_name, :work_background_position, :work_background_duration)',
           [
             ':name' => $attributes['name'],
             ':rate' => $attributes['rate'],
@@ -53,6 +54,7 @@ class CreateJobseekerForm extends Form
             ':summary' => $attributes['summary'],
             ':email' => $attributes['email'],
             ':password' => password_hash($attributes['password'], PASSWORD_DEFAULT),
+            ':birth_date' => $attributes['birth_date'],
             ':position' => $attributes['position'],
             ':work_background_company_name' => $prev_companies,
             ':work_background_position' => $prev_positions,
